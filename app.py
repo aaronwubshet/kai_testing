2bf34412a08552ffdba121f338b1a7685ba5f9d7
Aaron Wubshet <13680876+aaronwubshet@users.noreply.github.com>
2025-07-07 20:41:45 -0400

    production version in modular approach

--- app.py
+++ app.py
@@ -19,9 +19,9 @@ register_all_callbacks(app)
 
 if __name__ == "__main__":
     # For development
-    app.run(debug=True)
+    # app.run(debug=True)
     
     # For production (uncomment when deploying)
-    # import os
-    # port = int(os.environ.get("PORT", 8050))
-    # app.run(debug=True, host="0.0.0.0", port=port)
+    import os
+    port = int(os.environ.get("PORT", 8050))
+    app.run(debug=True, host="0.0.0.0", port=port)
